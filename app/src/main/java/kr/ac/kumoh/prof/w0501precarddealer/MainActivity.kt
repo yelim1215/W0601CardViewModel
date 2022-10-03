5720b091572478d3df9d508382589a2de6b65d4c
BWOH <bwoh@outlook.kr>
2022-10-04 01:22:59 +0900

    🎉 프로젝트 생성

---
+++ app/src/main/java/kr/ac/kumoh/prof/w0501precarddealer/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.prof.w0501precarddealer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
