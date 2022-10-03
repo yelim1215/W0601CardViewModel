e3e4315a1764f77d52c67551ecc0f091501dedb4
BWOH <bwoh@outlook.kr>
2022-10-04 02:24:26 +0900

    🚀 카드 딜러 완성

--- app/src/main/java/kr/ac/kumoh/prof/w0501precarddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/prof/w0501precarddealer/MainActivity.kt
@@ -2,7 +2,9 @@ package kr.ac.kumoh.prof.w0501precarddealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kr.ac.kumoh.prof.w0501precarddealer.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -12,6 +14,63 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         setContentView(binding.root)
 
-        binding.card1.setImageResource(R.drawable.c_2_of_hearts)
+        //binding.card1.setImageResource(R.drawable.c_2_of_hearts)
+//        val c = Random.nextInt(52)
+//        Log.i("Test", "$c : ${getCardName(c)}")
+//
+//        val res = resources.getIdentifier(
+//            getCardName(c),
+//            "drawable",
+//            packageName
+//        )
+//
+//        binding.card1.setImageResource(res)
+        binding.btnDeal.setOnClickListener {
+            val c = IntArray(5)
+            val res = IntArray(5)
+
+            //for (i in 0..4)
+            //for (i in 0 until 5)
+            //for (i in 0 until c.size)
+            for (i in c.indices) {
+                c[i] = Random.nextInt(52)
+
+                Log.i("Test", "${c[i]} : " +
+                        "${getCardName(c[i])}")
+
+                res[i] = resources.getIdentifier(
+                    getCardName(c[i]),
+                    "drawable",
+                    packageName
+                )
+            }
+
+            //card1.setImageResource(R.drawable.c_2_of_hearts)
+            binding.card1.setImageResource(res[0])
+            binding.card2.setImageResource(res[1])
+            binding.card3.setImageResource(res[2])
+            binding.card4.setImageResource(res[3])
+            binding.card5.setImageResource(res[4])
+        }
+    }
+
+    private fun getCardName(c: Int) : String {
+        val shape = when (c / 13) {
+            0 -> "spades"
+            1 -> "diamonds"
+            2 -> "hearts"
+            3 -> "clubs"
+            else -> "error"
+        }
+
+        val number = when (c % 13) {
+            0 -> "ace"
+            in 1..9 -> (c % 13 + 1).toString()
+            10 -> "jack"
+            11 -> "queen"
+            12 -> "king"
+            else -> "error"
+        }
+        return "c_${number}_of_${shape}"
     }
 }
