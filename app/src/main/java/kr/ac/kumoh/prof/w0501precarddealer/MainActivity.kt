172c2dfb531988dc8dde1a7cef57bd18f15b7460
BWOH <bwoh@outlook.kr>
2022-10-04 01:55:49 +0900

    🚧 레이아웃 분리 이전

--- app/src/main/java/kr/ac/kumoh/prof/w0501precarddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/prof/w0501precarddealer/MainActivity.kt
@@ -2,10 +2,16 @@ package kr.ac.kumoh.prof.w0501precarddealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.ac.kumoh.prof.w0501precarddealer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.card1.setImageResource(R.drawable.c_2_of_hearts)
     }
 }
